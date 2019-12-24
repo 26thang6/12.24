786071ffb9af12b7a28964172ddf12d3b31c7a6f
26thang6 <56572259+26thang6@users.noreply.github.com>
2019-12-24 11:49:15 +0800

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,9 @@
+<?php
+  
+    $imgCat=array('animals','architecture','nature','peole','tech');
+    for($x=0;$x<30;$x++)
+    {
+        $imgUrl="https://placeimg.com/30".($x%10)."/300/".$imgCat[rand(0,4)];
+        echo "<img src='" . $imgUrl . "'/>".PHP_EOL;
+    }
+?>
